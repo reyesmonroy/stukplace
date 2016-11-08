5ace37bdceb4bff971899e146d73e391f51dc11b
Deepak Sharma <sadysnaat@gmail.com>
2016-11-08 08:05:22 +0000

    Add very basic places search

--- app/models/place.rb
+++ app/models/place.rb
@@ -18,4 +18,12 @@ class Place < ActiveRecord::Base
   rescue ZeroDivisionError
     0
   end
+
+  def self.search(search)
+    if search
+      where(['name LIKE ? or address LIKE ?', "#{search}", "#{search}"])
+    else
+      all
+    end
+  end
 end
